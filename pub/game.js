6a7b1b8a3a80c3c5c6cf30a790c7ff3c49a5a4b9
Brian Zick <brian@zick.io>
2021-12-20 21:14:14 -0600

    shake animation first sketch

--- pub/game.js
+++ pub/game.js
@@ -1,33 +1,41 @@
-const max = 200
-let things = []
+let shakeFrame = 0
+let shake = [
+  [-5, 6, 2],
+  [-2, -6, 1],
+  [6, 2, 0],
+  [4, 3, 3],
+  [2, -6, 1]
+]
+let betweens = 0.25
 
 function setup() {
-  createCanvas(windowWidth, windowHeight)
-}
-
-function mouseMoved() {
-  store()
+  createCanvas(400, 400)
+  rectMode(CENTER)
+  angleMode(DEGREES)
+  frameRate(60)
 }
 
 function draw() {
   background(220)
-  things.forEach(i => {
-    circle(i[0], i[1], i[2]*2)
-  })
+  translate(width / 2, height / 2);
 
-  if (keyIsPressed) {
-    store()
+  if (shakeFrame != floor(shakeFrame)) {
+    nextFrame = shakeFrame + betweens
+    if (nextFrame > shake.length) {
+      nextFrame = 0
+    }
+    let r = lerp(shake[floor(shakeFrame)][0], shake[floor(nextFrame)][0], 0.5)
+    let x = lerp(shake[floor(shakeFrame)][1], shake[floor(nextFrame)][1], 0.5)
+    let y = lerp(shake[floor(shakeFrame)][2], shake[floor(nextFrame)][2], 0.5)
+    rotate(r)
+    rect(x, y, 50, 50)
+  } else {
+    rotate(shake[floor(shakeFrame)][0])
+    rect(shake[floor(shakeFrame)][1], shake[floor(shakeFrame)][2], 50, 50)
   }
-}
 
-function store() {
-  things.push([mouseX,mouseY,dist(pmouseX,pmouseY,mouseX,mouseY)])
-  
-  if (things.length > max) {
-    things.splice(0, 1)
+  shakeFrame += betweens
+  if (shakeFrame+1 > shake.length) {
+    shakeFrame = 0
   }
-
-  things.forEach(i => {
-    i[2] += dist(pmouseX, pmouseY, mouseX, mouseY) * 0.1;
-  })
 }
