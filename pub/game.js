bb80c03bd036decc12ff5065134db0b305529f49
Shipment22 <crapple2@datazap.net>
2021-12-20 22:49:58 -0500

    make animation do stuffs

--- pub/game.js
+++ pub/game.js
@@ -14,7 +14,8 @@ let animFrame = {
 
 	sz: 1
 }
-let speed = 0.4
+let violence = 16
+let mcSpeed = 0.4
 
 function setup() {
 	createCanvas(400, 400)
@@ -22,18 +23,37 @@ function setup() {
 	angleMode(DEGREES)
 	frameRate(60)
 
+	anim = []
+	for (let i = 0; i < 16; i ++) {
+		anim.push({
+			x: random(-violence, violence),
+			y: random(-violence, violence),
+
+			r: random(-violence, violence),
+
+			sz: map(i, 0, 16, 0.9, 1.1)
+		})
+	}
+
 	setInterval(update, 50)
 }
 
 function update() {
-
+	animIndex ++
+	animIndex = animIndex % anim.length
+	animFrame.x = lerp(animFrame.x, anim[animIndex].x, mcSpeed)
+	animFrame.y = lerp(animFrame.y, anim[animIndex].y, mcSpeed)
+	animFrame.r = lerp(animFrame.r, anim[animIndex].r, mcSpeed)
+	animFrame.sz = lerp(animFrame.sz, anim[animIndex].sz, mcSpeed)
 }
 
 function draw() {
 	background(220)
 
 	push()
-	translate(pos.x, pos.y)
+	translate(pos.x + animFrame.x, pos.y + animFrame.y)
+	rotate(animFrame.r)
+	scale(animFrame.sz)
 
 	rect(0, 0, w, h);
 	pop()
