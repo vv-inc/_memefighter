55026f04a2d275aec4c1d8797f8912f1b830e07f
Brian Zick <brian@zick.io>
2021-12-20 19:26:19 -0600

    weird crazy seashell pattern thing

--- pub/game.js
+++ pub/game.js
@@ -1,19 +1,29 @@
-let things = [];
+const max = 100
+let things = []
 
 function setup() {
   createCanvas(windowWidth, windowHeight)
 }
 
+function mouseMoved() {
+  store()
+}
+
+function keyPressed() {
+  store()
+}
+
 function draw() {
   background(220)
+  things.forEach(i => {
+    circle(i[0],i[1], (50-i[2] || 0) + 10)
+  })
+}
+
+function store() {
+  things.push([mouseX,mouseY,dist(pmouseX,pmouseY,mouseX,mouseY)])
   
-  things.push([mouseX,mouseY])
-  
-  if (things.length > 1000) {
+  if (things.length > max) {
     things.splice(0, 1)
   }
-  
-  things.forEach(i => {
-    circle(i[0],i[1], 250)
-  })
 }
